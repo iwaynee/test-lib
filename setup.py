eef79c5f81d21e504bc36bcae142dbef6897b88d
heinzeri <ivan.heinzer@contractors.roche.com>
2020-12-15 15:27:47 +0100

    test

---
+++ setup.py
@@ -0,0 +1,13 @@
+import re
+
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="test-lib",
+    version=1.0,
+    install_requires=[
+        "autobahn",
+        "twisted"
+    ]
+)
